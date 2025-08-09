1b586ba8135bb6349dd5cfff2b7c864d7797fa74
benwasden <was24001@byui.edu>
2025-08-09 14:04:32 -0700

    Edits

--- src/js/meal.js
+++ src/js/meal.js
@@ -1,14 +1,4 @@
-async function getRecipesByCategory(category) {
-  const response = await fetch(
-    `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`,
-  );
-  const mealData = await response.json();
-  console.log(mealData);
-  const meals = mealData.meals;
-  // console.log(meals);
-  // displayMeals(meals);
-  return meals;
-}
+import { getRecipesByCategory } from "./utils";
 
 const category = document.querySelector("#category");
 
