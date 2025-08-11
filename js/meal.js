cfdb80daacac630681a854fe593d43096227c827
benwasden <was24001@byui.edu>
2025-08-11 16:04:03 -0700

    JSON Implemented

--- js/meal.js
+++ js/meal.js
@@ -1,17 +1,20 @@
-import { getRecipesByCategory } from "./utils";
+import { getRecipesByCategory } from "./data";
 
+// Element selection for meal viewer
 const category = document.querySelector("#category");
-
 const mealList = document.querySelector("#meal-container");
+
+// Event listener on select dropdown to display the meals for the requested category
 category.addEventListener("change", async function () {
   mealList.innerHTML = "";
   const selectedValue = this.value;
   const mealInfo = await getRecipesByCategory(selectedValue);
-  console.log(mealInfo);
+  // console.log(mealInfo);
 
   displayMeals(mealInfo);
 });
 
+// Format to display meals, creating a card for each item
 const displayMeals = (meals) => {
   meals.forEach((meal) => {
     let item = document.createElement("li");
@@ -25,7 +28,16 @@ const displayMeals = (meals) => {
 
     item.appendChild(mealName);
     item.appendChild(mealImg);
+    item.setAttribute("class", "meal-item");
 
     mealList.appendChild(item);
   });
 };
+
+// const mealItem = document.querySelector(".meal-item");
+// const dialog = document.querySelector("#meal-dialog");
+
+// mealItem.addEventListener("click", async function () {
+//   dialog.innerHTML = "";
+//   const selectedMeal = this.value[0];
+// })
